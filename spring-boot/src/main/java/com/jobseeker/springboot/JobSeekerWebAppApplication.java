f5dd639d2ae50b3db51034fc5b8fc8ca74a296a2
Ekaterina Pykhteeva <106779677+Code1PK@users.noreply.github.com>
2022-10-31 19:47:18 -0400

    Adding Java Spring Boot

---
+++ spring-boot/src/main/java/com/jobseeker/springboot/JobSeekerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jobseeker.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekerWebAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/jobseeker/springboot/JobSeekerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseeker.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
