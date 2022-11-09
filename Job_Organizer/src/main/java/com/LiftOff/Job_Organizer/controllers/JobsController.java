eea9b403480b5242731468c323b77241e83cce33
Ekaterina Pykhteeva <106779677+Code1PK@users.noreply.github.com>
2022-11-09 10:34:10 -0500

    create jobs/add

---
+++ Job_Organizer/src/main/java/com/LiftOff/Job_Organizer/controllers/JobsController.java
@@ -0,0 +1,13 @@
+package com.LiftOff.Job_Organizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobsController {
+    @GetMapping("/jobs/add")
+    public String index() {
+        return "jobs/add";
+    }
+}
