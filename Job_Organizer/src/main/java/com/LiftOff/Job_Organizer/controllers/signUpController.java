ac5227b428bc777f11e4408c1f0ca67a05f4e1eb
Vitalii <106563944+Vit-Knysh@users.noreply.github.com>
2022-11-27 20:36:59 -0500

    Added Post Mapping for signUp

--- Job_Organizer/src/main/java/com/LiftOff/Job_Organizer/controllers/signUpController.java
+++ Job_Organizer/src/main/java/com/LiftOff/Job_Organizer/controllers/signUpController.java
@@ -1,14 +1,36 @@
 package com.LiftOff.Job_Organizer.controllers;
 
+import com.LiftOff.Job_Organizer.data.CustomerRepository;
+import com.LiftOff.Job_Organizer.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class signUpController {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @GetMapping("signUp")
     public String signUp() {
         return "signUp";
+    }
+
+    @PostMapping("signUp")
+    public String processSignUpForm(@ModelAttribute @Valid Customer newCustomer, Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "SignUp");
+            return "signUp";
+        }
+        customerRepository.save(newCustomer);
+        return"/";
     }
 
 }
