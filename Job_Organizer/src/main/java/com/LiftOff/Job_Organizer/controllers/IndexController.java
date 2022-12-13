4b105a2c84eb1e8a66ada76bdd26938ebe64ad5c
Uzo <uzoikejiofor@gmail.com>
2022-12-12 20:54:51 -0500

    LoginWelcomePage
    Log in welcome page

--- Job_Organizer/src/main/java/com/LiftOff/Job_Organizer/controllers/IndexController.java
+++ Job_Organizer/src/main/java/com/LiftOff/Job_Organizer/controllers/IndexController.java
@@ -2,6 +2,8 @@ package com.LiftOff.Job_Organizer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -9,5 +11,13 @@ public class IndexController {
     @GetMapping
     public String index() {
         return "index";
+    }
+
+
+    @PostMapping
+    public String index(@RequestParam String username,
+                                         @RequestParam String password){
+        return "loggingIn/welcomePage";
+
     }
 }
